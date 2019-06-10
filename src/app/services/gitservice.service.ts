d3527f1c2e066a80d9efa279bfd6f186b246dd86
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-10 12:41:35 +0300

    Updates form

--- src/app/services/gitservice.service.ts
+++ src/app/services/gitservice.service.ts
@@ -1,9 +1,18 @@
+
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { User } from './../user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitserviceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {}
+    getUser(name: string): Observable<User>{
+      const url= `https://api.github.com/users/${name}`;
+      return this.http.get<User>(url);
+    }
+
 }
